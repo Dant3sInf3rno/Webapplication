ee3843d99ada436bcbe27e71d8c1988181117455
Dant3sInf3rno <konstantin@lepschy.me>
2024-02-15 11:33:53 +0100

    Fehlerbehebung 1

---
+++ Test/main.js
@@ -0,0 +1,40 @@
+const chartData = {
+    labels: ["China", "USA", "Indien", "Russland", "Japan", "Iran", "Deutschland", "Saudi-Arabien", "Indonesien", "Südkorea", "Andere"],
+    data: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 45],
+  };
+  
+  const myChart = document.querySelector(".my-chart");
+  const ul = document.querySelector(".programming-stats .details ul");
+  
+  new Chart(myChart, {
+    type: "doughnut",
+    data: {
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: "CO2",
+          data: chartData.data,
+        },
+      ],
+    },
+    options: {
+      borderWidth: 10,
+      borderRadius: 2,
+      hoverBorderWidth: 0,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+  
+  const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+      let li = document.createElement("li");
+      li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+      ul.appendChild(li);
+    });
+  };
+  
+  populateUl();
