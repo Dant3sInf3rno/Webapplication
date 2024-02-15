e7968f5c5f5b2c181e9138c8d0303b99f9056e8e
Dant3sInf3rno <konstantin@lepschy.me>
2024-02-15 14:45:56 +0100

    Upload 15.02.2024/Anpassung

--- CO² - Footprinting/main.js
+++ CO² - Footprinting/main.js
@@ -1,16 +1,21 @@
-document.getElementById('searchInput').addEventListener('input', function () {
-    var searchValue = this.value.toLowerCase();
-    var countries = document.getElementById('countryList').getElementsByTagName('li');
+document.addEventListener('DOMContentLoaded', function () {
+  var searchInput = document.getElementById('searchInput');
+  var countryList = document.getElementById('countryList');
+  var countries = countryList.getElementsByTagName('li');
 
-    for (var i = 0; i < countries.length; i++) {
-        var countryName = countries[i].innerText.toLowerCase();
+  searchInput.addEventListener('input', function () {
+      var searchValue = this.value.toLowerCase();
 
-        if (countryName.includes(searchValue)) {
-            countries[i].style.display = 'block';
-        } else {
-            countries[i].style.display = 'none';
-        }
-    }
+      for (var i = 0; i < countries.length; i++) {
+          var countryName = countries[i].innerText.toLowerCase();
+
+          if (countryName.includes(searchValue)) {
+              countries[i].style.display = 'block';
+          } else {
+              countries[i].style.display = 'none';
+          }
+      }
+  });
 });
 
 const chartData = {
